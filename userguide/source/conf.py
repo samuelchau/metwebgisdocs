b9ade5084962a84221d7decd48458960fa863515
Samuel Chau <king@info-electronics.com>
2018-12-11 16:32:41 -0500

    Enabled numfig

--- userguide/source/conf.py
+++ userguide/source/conf.py
@@ -35,6 +35,7 @@ release = '0.0.1'
 #
 # needs_sphinx = '1.0'
 
+numfig = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
